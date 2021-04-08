bdc37a2e05d57459cb415b0e0c1a0732eac3477d
VanFleet0351 <50758972+VanFleet0351@users.noreply.github.com>
2021-04-07 21:39:51 -0400

    Added calculateGrandTotalDue and calculateSubTotaldue. Passing first 2 tests of grand and sub total using obvious arguments.

--- BabySitterKata/src/com/kylevanfleet/babysitter/NightlyPayCalculator.java
+++ BabySitterKata/src/com/kylevanfleet/babysitter/NightlyPayCalculator.java
@@ -1,5 +1,96 @@
 package com.kylevanfleet.babysitter;
 
-public class NightlyPayCalculator {
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
+public class NightlyPayCalculator {
+	
+	private double preBedRate = 12.00;
+	private double bedToMidnightRate = 8.00;
+	private double postMidnightRate = 16.00;
+	
+	private final LocalTime MIDNIGHT = LocalTime.parse("12:00AM", DateTimeFormatter.ofPattern("hh:mma"));
+	
+	/**
+	 * Default constructor for the NightlyPayCalculator.
+	 */
+	public NightlyPayCalculator() {
+		super();
+	}
+
+	/**
+	 * @param prebedRate
+	 * @param bedToMidnightRate
+	 * @param postMidnightRate
+	 */
+	public NightlyPayCalculator(double prebedRate, double bedToMidnightRate, double postMidnightRate) {
+		super();
+		this.preBedRate = prebedRate;
+		this.bedToMidnightRate = bedToMidnightRate;
+		this.postMidnightRate = postMidnightRate;
+	}
+	
+	public double calculateGrandTotalDue(LocalDateTime start, LocalDateTime bedTime, LocalDateTime end) {
+		double preBedTotal = 0f;
+		double bedToMidnightTotal = 0f;
+		double postMidnightTotal = 0f;
+		LocalDateTime midnightDateTime = LocalDateTime.of(start.toLocalDate().plusDays(1), MIDNIGHT);
+		
+		preBedTotal = calculateSubTotalDue(start, bedTime, preBedRate);
+		bedToMidnightTotal = calculateSubTotalDue(bedTime, midnightDateTime, bedToMidnightRate);
+		postMidnightTotal = calculateSubTotalDue(midnightDateTime, end, postMidnightRate);
+		
+		return preBedTotal + bedToMidnightTotal + postMidnightTotal;
+	}
+	
+	protected double calculateSubTotalDue(LocalDateTime start, LocalDateTime end, double rate) {
+		long hours = start.until(end, ChronoUnit.HOURS);
+		return rate * hours;
+	}
+	
+
+	/**
+	 * @return the prebedRate
+	 */
+	public double getPrebedRate() {
+		return preBedRate;
+	}
+
+	/**
+	 * @param prebedRate the prebedRate to set
+	 */
+	public void setPrebedRate(double prebedRate) {
+		this.preBedRate = prebedRate;
+	}
+
+	/**
+	 * @return the bedToMidnightRate
+	 */
+	public double getBedToMidnightRate() {
+		return bedToMidnightRate;
+	}
+
+	/**
+	 * @param bedToMidnightRate the bedToMidnightRate to set
+	 */
+	public void setBedToMidnightRate(double bedToMidnightRate) {
+		this.bedToMidnightRate = bedToMidnightRate;
+	}
+
+	/**
+	 * @return the postMidnightRate
+	 */
+	public double getPostMidnightRate() {
+		return postMidnightRate;
+	}
+
+	/**
+	 * @param postMidnightRate the postMidnightRate to set
+	 */
+	public void setPostMidnightRate(double postMidnightRate) {
+		this.postMidnightRate = postMidnightRate;
+	}
+	
 }
--- BabySitterKata/src/com/kylevanfleet/babysitter/NightlyPayCalculatorTest.java
+++ BabySitterKata/src/com/kylevanfleet/babysitter/NightlyPayCalculatorTest.java
@@ -2,13 +2,43 @@ package com.kylevanfleet.babysitter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 import org.junit.jupiter.api.Test;
 
 class NightlyPayCalculatorTest {
+	
+	//Error used for test dollar value returned
+	public final double EPSILON = 0.01;
+	public final LocalDate CURRENT_DATE = LocalDate.now();
+	public final LocalDate TOMORROWS_DATE = LocalDate.now().plusDays(1);
+	public final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("hh:mma");
+	public NightlyPayCalculator calc = new NightlyPayCalculator();
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCalculateGrandTotalDue() {
+		LocalTime startTime = LocalTime.parse("05:00PM", DATE_FORMAT);
+		LocalDateTime startDateTime = LocalDateTime.of(CURRENT_DATE, startTime);
+		LocalTime bedTime = LocalTime.parse("10:00PM", DATE_FORMAT);
+		LocalDateTime bedDateTime = LocalDateTime.of(CURRENT_DATE, bedTime);
+		LocalTime endTime = LocalTime.parse("04:00AM", DATE_FORMAT);
+		LocalDateTime endDateTime = LocalDateTime.of(TOMORROWS_DATE, endTime);
+		
+		assertEquals(140.00, calc.calculateGrandTotalDue(startDateTime, bedDateTime, endDateTime), EPSILON);
 	}
+	
+	@Test
+	void testCalculatePreBedTotalDue() {
+		LocalTime startTime = LocalTime.parse("05:00PM", DATE_FORMAT);
+		LocalDateTime startDateTime = LocalDateTime.of(CURRENT_DATE, startTime);
+		LocalTime bedTime = LocalTime.parse("10:00PM", DATE_FORMAT);
+		LocalDateTime bedDateTime = LocalDateTime.of(CURRENT_DATE, bedTime);
+		
+		assertEquals(60.00, calc.calculateSubTotalDue(startDateTime, bedDateTime, 12.00), EPSILON);
+	}
+	
 
 }
